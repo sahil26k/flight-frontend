a9f10a5645a50a6b8c77fdb026e854f3be345daf
Sahil <92850209+sahil26k@users.noreply.github.com>
2023-07-29 23:26:50 +0530

    Update Login.jsx

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,16 +1,20 @@
 import React, { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import axios from 'axios';
-import './login.css'
+import './login.css';
+
 const LoginPage = () => {
   const navigate = useNavigate();
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
   const [errorMessage, setErrorMessage] = useState('');
+  const [isLoading, setIsLoading] = useState(false); // Step 1: State for loader
 
   const handleLogin = (e) => {
     e.preventDefault();
-   
+
+    setIsLoading(true); // Step 2: Show loader when login process starts
+
     // Create a request body with the username and password
     const requestBody = {
       username,
@@ -20,14 +24,16 @@ const LoginPage = () => {
     // Send a POST request to the backend for authentication
     axios.post('https://flightapi-xdy0.onrender.com/authen', requestBody)
       .then((response) => {
+        setIsLoading(false); // Step 2: Hide loader when login process is complete
         const { success, message } = response.data;
 
         if (success) {
-          if(username==='admin' && password === 'admin'){
+          if (username === 'admin' && password === 'admin') {
             navigate('/adminhome');
           } else {
-          // Successful login
-          navigate('/userhome', { state: { username } });}
+            // Successful login
+            navigate('/userhome', { state: { username } });
+          }
         } else {
           // Invalid credentials
           setErrorMessage(message);
@@ -39,8 +45,9 @@ const LoginPage = () => {
       })
       .catch((error) => {
         console.error(error);
+        setIsLoading(false); // Step 2: Hide loader when login process encounters an error
       });
-  }; 
+  };
 
   return (
     <div className='login-container'>
@@ -65,7 +72,13 @@ const LoginPage = () => {
           required
         />
 
-        <button type="submit">Login</button>
+        {/* Step 3: Conditionally render the loader */}
+        {isLoading ? (
+          <div className="loader"></div>
+        ) : (
+          <button type="submit">Login</button>
+        )}
+
       </form>
 
       <p>Don't have an account? <Link to="/signup">Sign up</Link></p>
